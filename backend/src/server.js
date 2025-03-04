67a0c6b14fc397bc811657d39b159a52caebce96
MehdiIdoudi <mehdiaverus@gmail.com>
2025-03-04 09:49:35 +0100

    achraf push 9.49

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const stockRoutes = require("./routes/stockRoutes");
 const animalRoutes = require("./routes/animalRoutes");
-const pesticideRoutes = require('./routes/pesticideRoutes');
+const pesticideRoutes = require("./routes/pesticideRoutes");
 const app = express();
-const port = process.env.PORT;
-const cors = require("cors");
-const path = require('path');
+const port = process.env.Port;
+const path = require("path");
 const blogRoutes = require("./routes/blogRoutes");
-const userRoutes = require("./routes/userRoutes");
 const scanRoutes = require("./routes/scanRoutes");
 const conversationRoutes = require("./routes/conversationRoutes");
-
-
 
 // Middleware
 app.use(cors());
@@ -25,18 +21,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/users", userRoutes);
 app.use("/api/stocks", stockRoutes);
 app.use("/api/animals", animalRoutes);
-app.use('/api/pesticides', pesticideRoutes);
+app.use("/api/pesticides", pesticideRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ message: 'Something went wrong!' });
+  res.status(500).json({ message: "Something went wrong!" });
+});
 
 app.use("/api/blog", blogRoutes);
 
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-app.get('/test-image', (req, res) => {
+app.get("/test-image", (req, res) => {
   res.send(`
     <html>
       <body>
