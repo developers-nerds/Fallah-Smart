c8e9ee6f0eb36a01c8b0d5bdc958922b6d2a014f
achref-fraj <101942901+achref-fraj@users.noreply.github.com>
2025-03-18 19:18:10 -1100

    pushed

--- backend/src/routes/Education_RepliesRoute.js
+++ backend/src/routes/Education_RepliesRoute.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const Education_ReplyController = require('../controllers/Education_Replies');
 
 router.get('/', Education_ReplyController.getAllReplies);
+router.get('/question/:questionAndAnswerId', Education_ReplyController.getRepliesByQnAId);
 router.get('/:id', Education_ReplyController.getReplyById);
 router.post('/', Education_ReplyController.createReply);                        
+router.put('/:id/like', Education_ReplyController.likeReply);
 router.put('/:id', Education_ReplyController.updateReply);
 router.delete('/:id', Education_ReplyController.deleteReply);
-
 
 module.exports = router;
