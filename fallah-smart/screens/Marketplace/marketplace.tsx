8f666e232fe3a569825215049ab7230a2471ff12
achref-fraj <101942901+achref-fraj@users.noreply.github.com>
2025-03-26 00:10:18 -1100

    accepte khedmet ghassen

--- fallah-smart/screens/Marketplace/marketplace.tsx
+++ fallah-smart/screens/Marketplace/marketplace.tsx
@@ -392,15 +392,11 @@ const Marketplace = () => {
       case 'auction':
         return <AuctionHouse data={auctionData} />;
       case 'company':
-<<<<<<< HEAD
-        return <CompanyProfile />;
-=======
         return (
           <View style={{ flex: 1 }}>
             <CompanyProfile />
           </View>
         );
->>>>>>> 905226a30d962ec151dca3871d3a9f49960570be
       default:
         return <MarketplaceFeed data={products} />;
     }
